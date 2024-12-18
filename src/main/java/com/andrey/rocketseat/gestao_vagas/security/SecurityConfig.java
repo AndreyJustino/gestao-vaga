c45c2c66b471fd07fce4bfa0e1d80af34a125105
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-18 17:02:04 -0300

    feat: validacao de rota

---
+++ src/main/java/com/andrey/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.andrey.rocketseat.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    /*esse metodo por padrão ja é gerenciado pelo spring
+     mas o bean esta dizendo que agora que sobrescrever ele
+     */
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf((value) -> value.disable()) //desativando a protecao de inicio do security
+                .authorizeHttpRequests((auth -> {
+                    auth.requestMatchers("/candidate/").permitAll() //informando que qualquer um pode acessar essa rota
+                            .requestMatchers("/company/").permitAll()
+                            .anyRequest().authenticated();//aqui digo que as demais precisam de autenticacao
+                }));
+
+        return httpSecurity.build();
+    }
+
+    
+}
