602894a8b4282f8faf84a7dbbd2c37b4b7eb553b
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-18 13:01:14 -0300

    feat: enum usado na JobEntity

--- src/main/java/com/andrey/rocketseat/gestao_vagas/modules/company/Levels.java
+++ src/main/java/com/andrey/rocketseat/gestao_vagas/modules/company/Levels.java
@@ -1,8 +1,20 @@
 package com.andrey.rocketseat.gestao_vagas.modules.company;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Levels {
-    baixo,
-    medio,
-    alto;
+    ESTAGIARIO,
+    JUNIOR,
+    PLENO,
+    SENIOR;
+
+    @JsonCreator
+    public static Levels empty(String level){
+        if (level == null || level.trim().isEmpty()){
+            return null;
+        }
+
+        return Levels.valueOf(level.toUpperCase());
+    }
 
 }
