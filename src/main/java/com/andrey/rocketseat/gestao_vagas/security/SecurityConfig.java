ce7d6e43eec54f982fc64d2d87e8a559cab849c5
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-22 17:57:43 -0300

    feat: adicionando filtros a config principal

--- src/main/java/com/andrey/rocketseat/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/andrey/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.andrey.rocketseat.gestao_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,28 +11,33 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
     private SecurityFilter securityFilter;
+
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
 
     /*esse metodo por padrão ja é gerenciado pelo spring
      mas o bean esta dizendo que agora que sobrescrever ele
      */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf((value) -> value.disable()) //desativando a protecao de inicio do security
+        httpSecurity.csrf((value) -> value.disable())    //desativando a protecao de inicio do security
                 .authorizeHttpRequests((auth -> {
                     auth.requestMatchers("/candidate/").permitAll() //informando que qualquer um pode acessar essa rota
                             .requestMatchers("/company/").permitAll()
-                            .requestMatchers("/auth/company").permitAll()
-                            .requestMatchers("/auth/candidate").permitAll()
+                            .requestMatchers("/company/auth").permitAll()
+                            .requestMatchers("/candidate/auth").permitAll()
                             .anyRequest().authenticated();//aqui digo que as demais precisam de autenticacao
                 }))
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
                 ;
                 
-
+        System.out.println("===== Security Config ====");
         return httpSecurity.build();
     }
 
