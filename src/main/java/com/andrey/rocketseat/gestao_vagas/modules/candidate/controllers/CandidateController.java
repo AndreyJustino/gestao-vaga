82c244ddc58219ba68770b329ddb2d48af232d79
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-22 17:56:17 -0300

    feat: controller que retorna o perfil do usuario

--- src/main/java/com/andrey/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/andrey/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,10 +1,20 @@
 package com.andrey.rocketseat.gestao_vagas.modules.candidate.controllers;
 
 import com.andrey.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
+import com.andrey.rocketseat.gestao_vagas.modules.candidate.dto.CandidateProfileDTO;
 import com.andrey.rocketseat.gestao_vagas.modules.candidate.service.CreateCandidateService;
+import com.andrey.rocketseat.gestao_vagas.modules.candidate.service.ProfileCandidateService;
+
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +32,41 @@ public class CandidateController {
 
         try{
             Object result = this.createCandidateService.execute(candidate);
+
+            System.out.println("===== CandidateController try ====");
+
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
+            System.out.println("===== CandidateController catch ====");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
+    }
+
+    @Autowired
+    private ProfileCandidateService profileCandidateService;
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public ResponseEntity<Object> getProfileCandidate(HttpServletRequest request) {
+        
+        try{
+
+            String id = request.getAttribute("candidate_id").toString();
+
+            CandidateProfileDTO candidateProfileDTO = this.profileCandidateService
+                .execute(UUID.fromString(id));
+
+
+            System.out.println("===== Candidate Controller Profile try ====");    
+            return ResponseEntity.ok().body(candidateProfileDTO);
+
+
+        }catch(Exception e){
+            System.out.println("===== Candidate Controller Profile catch ====");
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).body("deu ruim aqui");
+        }
+        
     }
 }
