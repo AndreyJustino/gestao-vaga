8623914b28de6d4353793b6050b261bd5198fbc4
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-26 12:57:15 -0300

    feat: query para listar vagas

--- src/main/java/com/andrey/rocketseat/gestao_vagas/modules/company/repository/JobRepository.java
+++ src/main/java/com/andrey/rocketseat/gestao_vagas/modules/company/repository/JobRepository.java
@@ -3,7 +3,10 @@ package com.andrey.rocketseat.gestao_vagas.modules.company.repository;
 import com.andrey.rocketseat.gestao_vagas.modules.company.entities.JobsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobsEntity, UUID> {
+
+    List<JobsEntity> findByDescriptionContaining(String filter);
 }
